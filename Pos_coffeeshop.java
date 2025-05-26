2cda676a342daaea62acec563a03bdd71066933d
svtzoneph <accstream92@gmail.com>
2025-05-26 19:13:03 +0800

    Add files via upload

---
+++ Pos_coffeeshop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package coffee.pos_coffeeshop;
+
+/**
+ *
+ * @author jhonb
+ */
+public class Pos_coffeeshop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
