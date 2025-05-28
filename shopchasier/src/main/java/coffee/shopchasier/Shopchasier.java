fab2c9b3a4af110e7f0f7d508e1c8decd9b073cc
svtzoneph <accstream92@gmail.com>
2025-05-28 18:09:38 +0800

    Add files via upload

---
+++ shopchasier/src/main/java/coffee/shopchasier/Shopchasier.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package coffee.shopchasier;
+
+/**
+ *
+ * @author jhonb
+ */
+public class Shopchasier {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
